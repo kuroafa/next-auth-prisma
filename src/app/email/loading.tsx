ecf689ee398178f119bae47841f231ff92cb8ef0
Nader Abdulrub <nabdulrub16@gmail.com>
2023-08-27 22:23:10 -0400

    Added Loading to email

---
+++ src/app/email/loading.tsx
@@ -0,0 +1,14 @@
+import Loading from "@/components/Loading";
+import React from "react";
+
+type Props = {};
+
+const loading = (props: Props) => {
+  return (
+    <>
+      <Loading />
+    </>
+  );
+};
+
+export default loading;
